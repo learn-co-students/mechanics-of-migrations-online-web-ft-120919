4e3abda77a179c7e54fd3014721e7fcc478b02a4
Olusola Olagoroye <solaolagoroye@gmail.com>
2020-01-28 05:51:00 -0600

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,9 +1,7 @@
+# db/migrate/01_create_artists.rb
+
 class CreateArtists < ActiveRecord::Migration[5.2]
-    def up
-    end
-   
-    def down
-    end
+    
 
      def change
         create_table :artists do |t|
@@ -14,5 +12,16 @@ class CreateArtists < ActiveRecord::Migration[5.2]
     end
         
     end
+#     sql = <<-SQL
+#   CREATE TABLE IF NOT EXISTS artists (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT,
+#   genre TEXT,
+#   age INTEGER,
+#   hometown TEXT
+#   )
+# SQL
+
+# ActiveRecord::Base.connection.execute(sql)
   end
   
